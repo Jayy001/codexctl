4014a1c0b0a824a7d030cd717bed81bf5ff0f8f8
Nathaniel van Diepen <eeems@eeems.email>
2023-10-07 19:30:34 -0600

    Clean up script

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 set -eu
 
 opkg update
+
 echo "Installing basic build tools"
-opkg install gcc binutils busybox gawk ldd make sed tar
+opkg install gcc binutils busybox gawk ldd make sed tar patchelf
+
 echo "Installing recommended build tools"
 opkg install coreutils-install diffutils ldconfig patch pkg-config --force-overwrite
+
 echo "Installing automake, cmake, meson, and ninja"
-opkg install automake libintl-full libtool-bin cmake icu libopenssl bash git git-http python3-pip python3-setuptools coreutils-od
+opkg install automake libintl-full libtool-bin cmake icu libopenssl bash git git-http python3-pip python3-setuptools coreutils-od python3-psutil
 python3 -m pip install -U wheel
 cd /opt/tmp
 git clone https://github.com/ninja-build/ninja.git
@@ -18,28 +21,23 @@ CONFIG_SHELL=/opt/bin/bash python3 ./configure.py --bootstrap
 install -Dm0755 -t /opt/bin ./ninja
 cd /opt/tmp && rm -Rf /opt/tmp/ninja
 python3 -m pip install -U meson
+
 echo "Installing header files"
-for pkg in gcc libncurses-dev libxml2-dev python3-dev ruby-dev zlib-dev; do
-    if opkg list-installed "${pkg}"; then
-        opkg install "${pkg}" --force-overwrite --force-reinstall;
-    fi;
-done
+opkg install --force-overwrite --force-reinstall libncurses-dev libxml2-dev python3-dev ruby-dev zlib-dev
 /opt/bin/busybox wget -qO- "$(/opt/bin/busybox sed -Ene \
   's|^src/gz[[:space:]]entware[[:space:]]https?([[:graph:]]+)|http\1/include/include.tar.gz|p' \
   /opt/etc/opkg.conf)" | /opt/bin/busybox tar x -vzC /opt/include
 
 source /opt/bin/gcc_env.sh
 
+echo "Installing python dependencies"
 ln -s /opt/lib/libffi.so.8 /opt/lib/libffi.so
-
-python3 -m pip install -U pip setuptools python3-psutil
-
+python3 -m pip install -U pip setuptools
 git clone https://github.com/Jayy001/codexctl.git
 cd codexctl
 python3 -m pip install --extra-index-url extra-index-url=https://www.piwheels.org/simple -r requirements.txt nuitka
 
-opkg install patchelf
-
+echo "Building codexctl"
 python3 -m nuitka \
     --enable-plugin=pylint-warnings \
     --onefile \
