c8830c99c047ce3e7429c28cf2905d7df2dc370d
Jacob B <56607897+Jayy001@users.noreply.github.com>
2023-10-21 14:47:26 +0100

    Automatic ID retriving from github

---
+++ modules/extractor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from modules.update_metadata_pb2 import DeltaArchiveManifest 
+import sys, os, struct, bz2
+
+BLOCK_SIZE = 4096
+def extract(img, outname):
+    with open(outname,'wb') as out:
+        with open(img,'rb') as f:
+            magic = f.read(4)
+            if magic != b'CrAU':
+                raise "Wrong header"
+            major = struct.unpack('>Q',f.read(8))[0]
+            if major != 1:
+                raise "Unsupported version"
+            size = struct.unpack('>Q', f.read(8))[0]
+            manifest = f.read(size)
+            msg = DeltaArchiveManifest.FromString(manifest)
+            pos = f.tell()
+            written=0
+            for chunk in msg.install_operations:
+                f.seek(pos + chunk.data_offset)
+                data  = f.read(chunk.data_length)
+                dst_offset = chunk.dst_extents[0].start_block * BLOCK_SIZE
+                dst_length  = chunk.dst_extents[0].num_blocks * BLOCK_SIZE
+                if chunk.type == 1:
+                    data = bz2.decompress(data)
+                elif chunk.type == 0:
+                    print("offset:{}".format(dst_offset))
+                else:
+                    raise "Unsupported type " + chunk.type
+
+                padding = dst_length - len(data)
+                if (padding < 0):
+                    raise "Wrong length"
+                out.seek(dst_offset)
+                out.write(data)
+                out.write(b'\x00'*padding)
+                written += len(data)
+                print("\r{}".format(written), end="")
+
+            print()
+
--- modules/updates.py
+++ modules/updates.py
@@ -1,6 +1,7 @@
-import os, time, requests, re, uuid, sys
+import os, time, requests, re, uuid, sys, json
 
 from pathlib import Path
+from datetime import datetime
 
 import xml.etree.ElementTree as ET
 
@@ -18,64 +19,54 @@ class UpdateManager:
 		self.device_version = device_version if device_version else "3.2.3.1595"
   
 		self.logger.debug(f'Download folder is {self.DOWNLOAD_FOLDER}')
-  
-		self.id_lookups_rm1 = {
-			"3.6.1.1894": "CGaIOXfzAA",
-			"3.6.0.1865": "nnuJzg6Jj4-",
-			"3.5.2.1807": "UGWiACaUG0-",
-			"3.5.1.1798": "cR2nzMvbcW-",
-			"3.4.1.1790": "SgjzSG56eK-",
-			"3.4.0.1784": "yY5ri2SbLv-",
-			"3.3.2.1666": "Ak1sdSSGWD-",
-			"3.2.3.1595": "f99Lsgx3BG-",
-			"3.2.2.1581": "rsTV6U3FlH-",
-			"3.0.4.1305": "zo0ubu5Wle-",
-			"2.15.1.1189": "Xdvv3lBmE4-", 
-			"2.15.0.1067": "qbGuCuFIX7-",
-			"2.14.3.977": "uzKCb761HZ-",
-			"2.14.3.958": "DgsusjCrfd-",
-			"2.14.3.1047": "mQ3OMMnn0v-",
-			"2.14.3.1005": "nKHKu5aVAR-",
-			"2.14.1.866": "kPA6NrLNoo-",
-			"2.14.0.861": "r6qAtH141a-",
-			"2.13.0.758": "K7IjoRHW9V-",
-			"2.12.3.606": "DtbTMBso9w-",
-			"2.12.2.573": "F7KCxN9Zpp-",
-			"2.12.1.527": "xKvumVvxqC-",
-			"2.11.0.442": "3QEYXIWu4Z-",
-			"2.10.3.379": "2UgGBK40nD-",
-			"2.10.2.356": "Lp90j3g4at-",
-		}
-		self.id_lookups_rm2 = {
-			"3.6.1.1894": "T2dkdktE1H",
-			"3.6.0.1865": "7wgexMSZP5-",
-			"3.5.2.1807": "3bZjC0Xn5C-",
-			"3.5.1.1798": "9CfoVp8qCU-",
-			"3.4.1.1790": "rYfHxYmwC8-",
-			"3.4.0.1784": "fD3GCOcU9m-",
-			"3.3.2.1666": "ihUirIf133-",
-			"3.2.3.1595": "fTtpld3Mvn-",
-			"3.2.2.1581": "dwyp884gLP-",
-			"3.0.4.1305": "tdwbuIzhYP-",
-			"2.15.1.1189": "wVbHkgKisg-", 
-			"2.15.0.1067": "lyC7KAjjSB-",
-			"2.14.3.977": "joPqAABTAW-",
-			"2.14.3.958": "B7yhC887i1-",
-			"2.14.3.1047": "RGLmy8Jb39-",
-			"2.14.3.1005": "4HCZ7J2Bse-",
-			"2.14.1.866": "JLWa2mnXu1-",
-			"2.14.0.861": "TfpToxrexR-",
-			"2.13.0.758": "2N5B5nvpZ4-",
-			"2.12.3.606": "XOSYryyJXI-",
-			"2.12.2.573": "XnE1EL7ojK-",
-			"2.12.1.527": "8OkCxOJFn9-",
-			"2.11.0.442": "tJDDwsDM4V-",
-			"2.10.3.379": "n16KxgSfCk-",
-			"2.10.2.356": "JLB6Ax3hnJ-",
-		}
 
-		self.latest_toltec_version = "2.15.1.1189" 
+		versions = self.get_version_ids()
 
+		self.id_lookups_rm1 = versions['remarkable1']
+		self.id_lookups_rm2 = versions['remarkable2']
+
+		self.latest_toltec_version = versions['toltec']
+
+	def update_version_ids(self, location):
+		with open(location, 'w') as f:
+			try:
+				self.logger.debug('Downloading version-ids.json')
+				contents = requests.get('https://raw.githubusercontent.com/Jayy001/codexctl/main/data/version-ids.json').json()
+				json.dump(contents, f)
+			except requests.exceptions.Timeout:
+				raise SystemExit('Error: Connection timed out while downloading version-ids.json! Do you have an internet connection?')
+			except Exception as error:
+				raise SystemExit(f'Error: Unknown error while downloading version-ids.json! {error}')
+			
+	def get_version_ids(self):
+		if os.path.exists('data/version-ids.json'):
+			file_location = 'data/version-ids.json'
+			self.logger.debug("Found version-ids at data/version-ids.json")
+		else:
+			if os.name == 'nt':
+				folder_location = os.getenv('APPDATA') + '/codexctl'
+			elif os.name == 'posix':
+				folder_location = os.path.expanduser('~/.config')
+			
+			self.logger.debug(f'Folder location is {folder_location}')
+			if not os.path.exists(folder_location):
+				os.mkdir(folder_location)
+				
+			file_location = folder_location + 'version-ids.json'
+			
+			if not os.path.exists(file_location):
+				self.update_version_ids(file_location)
+		
+		with open(file_location, 'r') as f:
+			contents = json.load(f)
+			self.logger.debug(f'Contents are {contents}')
+
+			if contents['last-updated'] - int(datetime.now().timestamp()) > 2628000: # 1 month
+				self.update_version_ids(file_location)
+				contents = json.load(f)
+
+			return contents
+		
 	def get_version(self, device=2, version=None, download_folder=None):
 		if download_folder is None:
 			download_folder = self.DOWNLOAD_FOLDER
@@ -178,7 +169,7 @@ class UpdateManager:
 			tries += 1
 
 			self.logger.debug(f'Sending POST request to {self.updates_url} with data {data} [Try {tries}]')
-                     
+					 
 			response = requests.post(self.updates_url, data)
 
 			if response.status_code == 429:
@@ -237,7 +228,7 @@ class UpdateManager:
 				done = int(50 * dl / total_length)
 				sys.stdout.write("\r[%s%s]" % ("=" * done, " " * (50 - done)))
 				sys.stdout.flush()
-    
+	
 		print(end="\r\n")
 
 		self.logger.debug(f"Downloaded {download_folder}/{name}")
