47f86bc231dd69ca39cd26ed8b76e9af7913dfa0
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2023-12-03 15:47:22 -0700

    Fix extra-index-url

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ ln -s /opt/lib/libffi.so.8 /opt/lib/libffi.so
 python3 -m pip install -U pip setuptools
 git clone https://github.com/Jayy001/codexctl.git
 cd codexctl
-python3 -m pip install --extra-index-url extra-index-url=https://www.piwheels.org/simple -r requirements.txt nuitka
+python3 -m pip install --extra-index-url=https://www.piwheels.org/simple -r requirements.txt nuitka
 
 echo "Building codexctl"
 python3 -m nuitka \
