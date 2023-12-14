4dbf5c992c18ecb8c28ba1038518a2c4009e2eba
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-12-13 19:44:21 -0700

    Get build working

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -3,6 +3,6 @@ set -e
 
 run_vm.sh -serial null -daemonize
 wait_ssh.sh
-scp -r /src root@localhost:/opt/tmp
-ssh root@localhost '/opt/bin/build.sh'
+rsync -avr --exclude ".git" /src root@localhost:/opt/tmp
+ssh root@localhost 'bash -l -c /opt/bin/build.sh'
 save_vm.sh
